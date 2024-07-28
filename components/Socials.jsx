456f4d65067f803563205667ef8fe927dfdd6652
Aniket Thakre <aniket.thakre1997@gmail.com>
2024-07-28 14:23:27 +0530

    Social links added

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,22 +1,27 @@
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn , FaYoutube, FaTwitter} from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const social = [
-  {icon: <FaGithub />, path: ""},
-  {icon: <FaLinkedinIn />, path: ""},
-  {icon: <FaYoutube />, path: ""},
-  {icon: <FaTwitter />, path: ""},
-]
-const Socials = ({containerStyles, iconStyles}) => {
+  {
+    icon: <FaLinkedinIn />,
+    path: "https://www.linkedin.com/in/aniket-thakre-b8a632165/",
+  },
+  { icon: <FaGithub />, path: "https://github.com/AniketThakre" },
+  { icon: <FaTwitter />, path: "https://x.com/AniketThakre8" },
+  // {icon: <FaYoutube />, path: ""},
+];
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-      {social.map((item, index)=> {
-        return <Link key={index} href={item.path} className={iconStyles}>
-          {item.icon}
-        </Link>
+      {social.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
       })}
     </div>
   );
-}
+};
 
-export default Socials
+export default Socials;
